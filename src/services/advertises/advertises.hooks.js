229a5ece0c14866e016ef76210fff6e4b6c16fb9
Saeed Bagheri <saeed.bagheri903@gmail.com>
2018-11-15 16:48:21 +0330

    add Feathers-authentication-hooks to Avertises hook

--- src/services/advertises/advertises.hooks.js
+++ src/services/advertises/advertises.hooks.js
@@ -1,14 +1,16 @@
 
+const { authenticate } = require('@feathersjs/authentication').hooks;
+const hooks = require('feathers-authentication-hooks');
 
 module.exports = {
   before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: []
+    all: [authenticate('jwt')],
+    find: [hooks.restrictToOwner({ idField: 'Id', ownerField: 'ownerId' })],
+    get: [hooks.restrictToOwner({ idField: 'Id', ownerField: 'ownerId' })],
+    create: [hooks.associateCurrentUser({ idField: 'Id', as: 'ownerId'})],
+    update: [hooks.restrictToOwner({ idField: 'Id', ownerField: 'ownerId' })],
+    patch: [hooks.restrictToOwner({ idField: 'Id', ownerField: 'ownerId' })],
+    remove: [hooks.restrictToOwner({ idField: 'Id', ownerField: 'ownerId' })]
   },
 
   after: {
